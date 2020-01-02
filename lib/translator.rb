2de6bd0c7e2537dca5c9e729c6fde5ceb805ae47
Thomas Humphrey <thumphrey9123@gmail.com>
2020-01-02 18:52:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,15 +5,10 @@ require 'pp'
 
 def load_library(file)
   emoticons = YAML.load_file(file)
-  # hash = {
-  # get_meaning => {},
-  # get_emoticon => {}
-  # }
-  # new_hash
-  # emoticons.each {|k,v| new_hash[v[1]] = k }
-  # emoticons.each {|k,v| hash[get_emoticon][v[0]] = v[1]}
-  pp emoticons
-  #hash
+  hash = {:get_meaning => {},:get_emoticon => {}}
+  emoticon.each {|k,v| hash[:get_meaning][v[1]] = k}
+  emoticon.each {|k,v| hash[:get_emoticon][v[0]] = v[1]}
+  hash
 end
 
 def get_japanese_emoticon(file, emoticon)
