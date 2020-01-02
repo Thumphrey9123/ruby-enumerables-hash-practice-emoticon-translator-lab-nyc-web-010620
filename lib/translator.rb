f895e48a143079e0a5e4167cf611bb556e559163
Thomas Humphrey <thumphrey9123@gmail.com>
2020-01-02 17:44:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,31 @@
 # require modules here
+require "yaml"
+require 'pp'
 
-def load_library
-  # code goes here
+
+def load_library(file)
+  emoticons = YAML.load_file(file)
+  hash = {
+  #get_meaning => { emoticons.each {|k,v| v[1], k}},
+  #get_emoticon => {emoticons.each {|k,v| v[0],v[1]}}
+  }
+  hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, emoticon)
+  lib = load_library(file)
+  if lib[emoticon]
+    return lib[emoticon]
+  else
+    return "Sorry a translation cannot be found"
+end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file, emoticon)
+  lib = load_library(file)
+  if lib[emoticon]
+    return lib[emoticon]
+  else
+    return "Sorry a translation cannot be found"
+  end
 end
