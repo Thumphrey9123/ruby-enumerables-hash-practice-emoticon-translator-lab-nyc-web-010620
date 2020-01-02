d58c5aa59d0e7c37b174a2cfcf9a268b0bcab1dc
Thomas Humphrey <thumphrey9123@gmail.com>
2020-01-02 17:56:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,9 +5,11 @@ require 'pp'
 
 def load_library(file)
   emoticons = YAML.load_file(file)
+  getMeaning = emoticons.each_with_index {|v,i| v[1], k}
+  getEmoticon = emoticons.each_with_index {|v,i| v[0],v[1]}
   hash = {
-  get_meaning => { emoticons.each_with_index {|k,v| v[1], k}},
-  get_emoticon => {emoticons.each_with_index {|k,v| v[0],v[1]}}
+  get_meaning => { m },
+  get_emoticon => { e }
   }
   hash
 end
