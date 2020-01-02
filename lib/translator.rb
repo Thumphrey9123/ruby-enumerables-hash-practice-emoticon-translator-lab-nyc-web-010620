b04df488355e5faf2cf1fd52496e867e8b9fc38d
Thomas Humphrey <thumphrey9123@gmail.com>
2020-01-02 18:22:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,6 +12,7 @@ def load_library(file)
   emoticons.each {|k,v| hash[get_meaning][v[1]] = k }
   emoticons.each {|k,v| hash[get_emoticon][v[0]] = v[1]}
   pp hash
+  hash
 end
 
 def get_japanese_emoticon(file, emoticon)
